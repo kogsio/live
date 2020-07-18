3a10fab7917410cd73be3cca82d0e6da40d41485
abelsan <abel@mit.edu>
2020-07-17 21:37:28 -0400

    styles hw code

--- components.js
+++ components.js
@@ -13,15 +13,32 @@ class Submodule extends HTMLElement {
     const exercise    = this.getAttribute('exercise');
     const exerciseUrl = this.getAttribute('exerciseUrl');
     const test        = this.getAttribute('test');
-    const testUrl     = this.getAttribute('testUrl');    
+    const testUrl     = this.getAttribute('testUrl');
+
+    let videoStr = '';
+    if(video){
+      videoStr = `<a href="${videoUrl}"><i class="fas fa-video"></i> ${video}</a><br />`;
+    }
+    let camtasiaStr = '';
+    if(camtasia){
+      camtasiaStr = `<a href="${camtasiaUrl}"><i class="fab fa-cuttlefish"></i> ${camtasia}</a><br />`;
+    }
+    let exerciseStr = '';
+    if(exercise){
+      exerciseStr = `<a href="${exerciseUrl}"> <i class="fas fa-tools"></i> ${exercise}</a><br />`;
+    }
+    let testStr = '';
+    if(test){
+      testStr = `<a href="${testUrl}"> <i class="fas fa-vial"></i> ${test}</a><br/>`;
+    }
 
     this.innerHTML = `
         <h5>${title}</h5>
         <p>
-          <a href="${videoUrl}"><i class="fas fa-video"></i> ${video}</a><br />
-          <a href="${camtasiaUrl}"><i class="fab fa-cuttlefish"></i> ${camtasia}</a><br />            
-          <a href="${exerciseUrl}"> <i class="fas fa-tools"></i> ${exercise}</a><br />
-          <a href="${testUrl}"> <i class="fas fa-vial"></i> ${test}</a><br/>
+          ${videoStr}
+          ${camtasiaStr}
+          ${exerciseStr}
+          ${testStr}          
         </p>
     `;    
 
